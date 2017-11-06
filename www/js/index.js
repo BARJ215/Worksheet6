05a015626f6b0ece41d8848efeaa9da3b38d8d4d
BARJ215 <barj2_15@uni.worc.ac.uk>
2017-11-06 14:31:57 +0000

    StoreText

--- www/js/index.js
+++ www/js/index.js
@@ -8,8 +8,11 @@ $(document).on("pagecreate","#pageone",function(){
 function submitText() {
 	var text = $('#textinput').val();
 	alert(text);
+    var textKey= "buttonText";
+    storeValue(textKey,text);
 }
 
 function storeValue(key, value) {
-	//add some code to store the key-value pair in persistant storage 
+	//add some code to store the key-value pair in persistant storage
+    window.localStorage.setItem(key,value);
 }
